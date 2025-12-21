ad641408a432333430136e23b9ed3b67b0583e3e
pylover <vahid.mardani@gmail.com>
2025-12-21 20:10:57 +0330

    Fix: copyrigth and naming

--- src/monad.h
+++ src/monad.h
@@ -1,5 +1,23 @@
-#ifndef CORE_MONAD_H_
-#define CORE_MONAD_H_
+// Copyright 2025 Vahid Mardani
+/*
+ * This file is part of cmonad.
+ *  cmonad is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.
+ *
+ *  cmonad is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with cmonad. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Author: Vahid Mardani <vahid.mardani@gmail.com>
+ */
+#ifndef SRC_MONAD_H_
+#define SRC_MONAD_H_
 /* generic stuff (must included once) */
 #define LNAME_PASTE(x, y) x ## y
 #define LNAME_EVAL(x, y)  LNAME_PASTE(x, y)
@@ -12,7 +30,7 @@
 #define MONAD_CHAIN(a, b) (a)->next = b
 
 
-#endif  // CORE_MONAD_H_
+#endif  // SRC_MONAD_H_
 
 
 /* generic<template> definitions */
